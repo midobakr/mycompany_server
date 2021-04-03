ccea70a9d213c2a218a4e926d251438c6b6834b8
bakr <you@example.com>
2021-04-03 09:36:58 +0200

    fix deployment

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ webpush.setVapidDetails(
 )
 
 //app.listen(3333,arr[arr.length-1].address,()=>{
-  app.listen(3333,()=>{
-    console.log('server is up on port : ',arr[arr.length-1].address)
+  app.listen(process.env.PORT,()=>{
+//    console.log('server is up on port : ',arr[arr.length-1].address)
   
     connectToDB()
 }) 
